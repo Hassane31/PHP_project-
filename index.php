58c30a7a42585d27a6e81fddfdc7d92c84bda369
hassane31 <hassane31@kali.SAMSUNG>
2026-01-19 23:41:02 +0100

    comment cours

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-echo 'welcome';
-echo '<br>'
-?>
+<?php
 
-<?='<h1>hello world';?>
-</body>
-</html>
+//single line comment 
+#single line comment 
+/*
+Line 1
+Line 2
+*/
+echo 'hello php';
+
+
+/*
+=======================
+== MY Special App======
+=======================
+*/
