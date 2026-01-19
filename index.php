c5992d8d3a2028d2d8f2db7e89fff010c2f25110
hassane31 <hassane31@kali.SAMSUNG>
2026-01-19 23:08:48 +0100

    end cours

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-echo "It works!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+echo 'welcome';
+echo '<br>'
+?>
+
+<?='<h1>hello world';?>
+</body>
+</html>
