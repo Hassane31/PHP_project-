0a0f2c5554ef05cc435a048e0951b77a4d9c90d8
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-01-22 22:47:07 +0100

    using variables

--- index.php
+++ index.php
@@ -1,55 +1,14 @@
-<?php
-//Array With Complex Tests
-echo '<pre>';
-print_r([
-0 => "ahmed",
-"A"=>"mhmood",
-"B"=>"warii",
-"hassane"
-]);
-echo '<pre>';
-echo '<br>';
-
-
-echo '<pre>';
-print_r([
-0 => "ahmed",
-"A"=>"mhmood",
-"B"=>"warii",
-"hassane",
-"mhmhd" => [
-    "1",
-    "2",
-    "3"
-]
-]);
-echo '<pre>';
-
-
-
-//display
-/*
-Array
-(
-    [0] => ahmed
-    [A] => mhmood
-    [B] => warii
-    [1] => hassane
-)
-
-
-Array
-(
-    [0] => ahmed
-    [A] => mhmood
-    [B] => warii
-    [1] => hassane
-    [mhmhd] => Array
-        (
-            [0] => 1
-            [1] => 2
-            [2] => 3
-        )
-
-)
- */
+<?php $username = "hassane"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my page <?php echo $username ?></title>
+</head>
+<body>
+    <div>hi my name is <?php echo $username ?></div>
+    <div>your score is 1000 points</div>
+    <div>score for <?php include("score.php");?></div>
+</body>
+</html>
