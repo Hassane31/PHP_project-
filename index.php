8652a80da8eda6891db9a0004d22bbceac4a5712
hassane31 <hassane31@kali.SAMSUNG>
2026-01-23 23:40:10 +0100

    //Predefined And Magic Constants And Reserved Keywords

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
-//Introduction To Constants
-define("DB_NAME","hassane",true);
-define('MAIN_NBR',5);
+//Predefined And Magic Constants And Reserved Keywords
 
-echo DB_NAME;
-echo MAIN_NBR * 10 ;
+echo php_uname();  //show OS information
+
+echo '<br>';
+echo PHP_VERSION;//show version php
+echo '<br>';
+echo PHP_OS_FAMILY; //os
+echo '<br>';
+echo PHP_INT_MAX; // max integer
+echo '<br>';
+echo DEFAULT_INCLUDE_PATH;//show pTH of file php {/opt/lampp}
+echo '<br>';
+echo __FILE__;//project file 
+echo '<br>';
+echo __LINE__;
+echo '<br>';
+//list of reserved words (exemple :BREAK)
+echo '<br>';
+
+//no problem here 
+function hello(){
+    return 'hello';
+}
+echo hello ();
+
+//reserved word 
+function clone(){
+    return 'clone';
+}
+echo clone();
