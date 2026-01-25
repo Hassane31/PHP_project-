da65f72f47929bb107acf092aa547e06995199d4
hassane31 <hassane31@kali.SAMSUNG>
2026-01-25 22:23:02 +0100

    Logical Operators

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php 
-//Increment And Decrement Operators
-$like = 0;
-$like++;
-$like++;
-$like++;
-$like++;
-$like--;
-echo $like ;
-$like--;
-$like--;
-$like--;
-$like--;
+//logical Operators 
+
+/*
+&&  =>  and 
+||  =>  or 
+xor =>  Xor 
+!   =>  Not
+*/
+
+var_dump(100 > 50 and 20 =='20');//true
 echo '<br>';
-echo $like ;
-
-
+var_dump(100 > 50 && 20 =='220');//false
 echo '<br>';
-$a =0 ;
-echo $a++; //0
+var_dump(100 > 50 or 20 =='20');//true
 echo '<br>';
-echo $a ; //1
-
-
+var_dump(100 > 50 || 20 =='210');//true
+echo '<br>';
+var_dump(100 > 50 xor 20 =='210');//true
+echo '<br>';
+var_dump(100 > 50 xor 20 =='20');//false
+echo '<br>';
+  
