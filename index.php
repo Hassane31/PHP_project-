541d38ad617c8ad6847f1dd0e65ef838422accfb
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-01-26 23:04:28 +0100

    If, Elseif, Else - Basics

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
-<?php
+<?php 
+//If, Elseif, Else - Basics
 
-//opreratorr precedence
-/**
- * Operator precedence
- * || has Greater precedence then OR 
- * && has Greater precedence then AND
- */
+if ( 10 > 5){
+    echo "hello world ";
+}
 
-echo 2+2+2*3 ;//2+2+6....
-echo '<br>';
-echo 10 || false ; // 1
-echo '<br>';
-echo 10 || false || "" || '' || 0 ;// 1 
-echo '<br>';
-echo var_dump(10 || false );
-echo '<br>';
-$a = 10 || false ; //$a = (10 || false) => 1
-echo $a ; // 1 
-echo '<br>';
-$b = 10 or false ; //($b = 10) or false => 1 
-echo '<br>';
-echo $b ; // 10 
-
+echo '<br >';
+if ( 10 < 10){
+    echo "first Condition !";
+}
+elseif(10 == 10 ){
+    echo "second Condition! ";
+}
+else{ //10>10 
+    echo "No 2";
+}
