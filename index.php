d70ec4dcad6ec7b0d493bdbd44f79ad09a508dd4
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-01-28 01:57:22 +0100

    switch

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
-<?php 
-//ternary conditional operator 
-$a = 2;
-$feeling = $a ;
-$result = $a ;
-
-//echo $a == 3 ? "good" :"bed";
-
-echo "<br>";
-
-echo "I ".($a>10 ? "love":"hate")." Php because its a ".($a>10 ? "good":"bed")." languge";
-
-echo "<br>";
+<?php
+//Switch 
 
 
-$feeling = $a > 10 ? "love":"hate";
-$result  = $a > 10 ? "good":"bed";
+$day = "sat";
 
-echo "I ".$feeling." Php because its a ".$result." languge";
-
+switch($day){
+    case "sat" :
+      echo "Hello to day is $day we are open From 10:20 am";
+      break; 
+    case "sun":
+    case "mon":
+    case "thus":
+    case "wed":
+      echo "Hello to day is $day we are open From 8:00 am";
+      break;
+    case "thar" :
+      echo "Hello to day is $day we are open From 10:00 am";
+    break;
+default : echo "Unknown day !!! ";
+}
