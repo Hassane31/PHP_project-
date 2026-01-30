2885d7de3177b0e2fd9eed29123b4bb0f84dd0f7
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-01-30 22:40:06 +0100

    function and echo

--- index.php
+++ index.php
@@ -1,34 +1,14 @@
-<?php
-//Advanced Examples With Parameters And Arguments
+<?php 
+//function return and echo 
 
+$prizes=["PC","playstation","XBOX","Appel","TV","laptop","ipad","iphone"];
 
-
-
-function say_hello($one,$two){
-    echo "hello sir $one and $two<br>";
-
-}
-
-say_hello("hassane","iness");
-
-
-function deep_frez($item){
-    if($item == "water"){
-        echo "make ace <br>";
+function get_num($num1,$num2){
+    return $num1 + $num2 ;
+    echo 'will not echo anything';
     }
-
-    elseif($item == "cola"){
-        echo "make cold <br>";
-    }
-
-    elseif($item == "fruits"){
-        echo "make fresh <br>";
-    }
-    else{
-        echo "unknown item <br>";
-    }
-}
-
-deep_frez("fruits");
-deep_frez("cola");
-deep_frez("water");
+// get_num(1,2);//3
+// $prizes[3];
+ echo '<br>';
+$prize_number = get_num(1,2);
+echo $prizes[$prize_number];
