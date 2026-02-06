bdbfb8e514e81cdfd8238c3c59fe0f98c6fcd9f3
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-06 21:44:26 +0100

    built in string function

--- index.php
+++ index.php
@@ -1,59 +1,39 @@
 <?php
-////string functions part 2 
-$friends = ["hassane","mhmd","iness","yns"]; 
-echo implode(" ",$friends)."<br>";
-echo implode("< ",$friends)."<br>";
-echo implode("@@",$friends)."<br>";
-echo implode($friends)."<br>";
+//Built in String Function
 
-$str = "I hate laabane hassane";
+echo chunk_split("laabane hassane ",3,"@");//laa@ban@e h@ass@ane@ @
+echo"<br>";
+
 echo "<pre>";
-print_r(explode(" ", $str));
-echo "</pre><br>";
+print_r(str_split("Hassane"));
+echo "<pre>";
+
+//display
+
+    // [0] => H
+    // [1] => a
+    // [2] => s
+    // [3] => s
+    // [4] => a
+    // [5] => n
+    // [6] => e
+
+echo "<pre>";
+print_r(str_split("Hassane",2));
+echo "<pre>";
+
 //display 
-//     [0] => I
-//     [1] => hate
-//     [2] => laabane
-//     [3] => hassane
 
-echo "<pre>";
-print_r(explode("l", $str));
-echo "</pre>";
+    // [0] => Ha
+    // [1] => ss
+    // [2] => an
+    // [3] => e
 
-//dislay 
-//     [0] => I hate 
-//     [1] => aabane hassane
 
-echo "<pre>";
-print_r(explode(" ", $str,3));
-echo "</pre>";
-
-// display 
-//     [0] => I
-//     [1] => hate
-//     [2] => laabane hassane
-
-echo "<pre>";
-print_r(explode(" ", $str,-2));
-echo "</pre><br>";
-
-// display 
-//     [0] => I
-//     [1] => hate
-
-echo str_shuffle("Hassane")."<br>";
-echo strrev("Hassane")."<br>";
-
-echo strlen("    hassane    ");// 15
+echo "<h1>hello iness</h1>";
 echo "<br>";
-echo strlen(trim("    hassane    "));// 7 
+echo strip_tags("<h1>hello iness</h1>");//delete all tags
 echo "<br>";
-echo trim("###hassane##","#");//hassane
+echo strip_tags("<h1>hello iness</h1>","<h1>");//delete all tags except <h1>
 echo "<br>";
-echo trim("###hassane##","#@");//same with 
-echo "<br>";
-echo trim("#@@@##hassane#@#","@");//no changes  
-echo "<br>";
-echo rtrim("###hassane##","#");//###hassane
-echo "<br>";
-echo ltrim("###hassane##","#");//hassane##
+echo nl2br("laabane\nhassane\ngoode\nguy");
