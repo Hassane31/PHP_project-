39aa80572c115f3b047f9dea81d9b7d021f4fb89
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-18 17:21:38 +0100

    String Functions part 8

--- index.php
+++ index.php
@@ -1,64 +1,44 @@
 <?php
-//String Functions str_replace str_ireplace 
-//syntax 
+//string functions 
 
-//str_replace (Find[required] , replace[required],Data[required],replace count [Opitional])
-echo str_replace("@","a","L@@b@ne H@ss@ne",$r);//----> Laabane Hassane
-echo "<br> Reaplaces count : ".$r;//---> 5
+$str = "welcome to php programming";
+
+echo strlen($str); // 26
 echo "<br>";
 
+echo wordwrap($str,3,"<br>");
 
-echo str_replace(["@","#"],"a","L@#b#ne H@ss#ne",$r);//----> Laabane Hassane
+// welcome
+// to
+// php
+// programming
+
+echo wordwrap($str,3,"<br>",true);
+
+// wel
+// com
+// e
+// to
+// php
+// pro
+// gra
+// mmi
+// ng
+
+echo "<br>";
+echo ord("a"); // 97
+echo "<br>";
+echo ord("A"); // 65
+echo "<br>";
+echo chr(97); // a
 echo "<br>";
 
-echo str_replace(["@","#"],["l","a"],"L@##@@# H@ss#ne",$r);//----> Llaalla Hlssane
+echo similar_text("programming","programmingo");//11
 echo "<br>";
 
-echo "<pre>";
-print_r(str_replace("One",1,["One","Two","Three","One","One"]));
-echo "</pre>";
+echo similar_text("programming","programmingo",$percent); 
+echo "<br>";
 
-    // [0] => 1
-    // [1] => Two
-    // [2] => Three
-    // [3] => 1
-    // [4] => 1
+echo $percent; // 95.652173913043
 
-
-echo "<pre>";
-print_r(str_replace(["One","Two"],[1,2],["One","Two","Three","One","One"]));
-echo "</pre>";
-
-    // [0] => 1
-    // [1] => 2
-    // [2] => Three
-    // [3] => 1
-    // [4] => 1
-
-echo "<pre>";
-print_r(str_replace(["One","Two","Three"],[1,2],["One","Two","Three","One","One"]));
-echo "</pre>";
-
-    // [0] => 1
-    // [1] => 2
-    // [2] => 
-    // [3] => 1
-    // [4] => 1
-
-
-echo "<pre>";
-print_r(str_replace(["one","two"],[1,2],["One","Two","Three","One","One"]));
-echo "</pre>";
-
-  //no changes cuz its sensitive string
-//use ireplace 
-echo "<pre>";
-print_r(str_ireplace(["one","two"],[1,2],["One","Two","Three","One","One"]));
-echo "</pre>";
-
-    // [0] => 1
-    // [1] => 2
-    // [2] => Three
-    // [3] => 1
-    // [4] => 1
-
+echo "<br>";
