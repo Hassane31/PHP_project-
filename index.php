33180b8466ccfdb150e06b8a5eae511a3104dd9f
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-19 17:49:58 +0100

    Array = marge , replace , rand , shuffle

--- index.php
+++ index.php
@@ -1,24 +1,77 @@
 <?php
-//Current, Next, Prev, Reset, End
+//Array marge ,Array replace ,array rand ,Shuffle
 
-$friends = ["Alice", "Bob", "Charlie", "David", "Eve"];
+$marge_one = ["One"=>"PHP","Two"=>"Java","Three"=>"Python"];
+$marge_two = ["One"=>"C++","Four"=>"C#","Five"=>"JavaScript"];
 
-echo "Current: " . current($friends) . "<br>"; // Alice
-echo "Next: " . next($friends) . "<br>"; // Bob
-echo "Current: " . current($friends) . "<br>"; // bob
-echo "Next: " . next($friends) . "<br>"; // Charlie
-echo "Prev: " . prev($friends) . "<br>"; // Bob
-echo "Reset: " . reset($friends) . "<br>"; // Alice
-echo "End: " . end($friends) . "<br>"; // Eve
-echo "Current: " . current($friends) . "<br>"; // Eve
+echo "<pre>";
+print_r(array_merge($marge_one,$marge_two));
+echo "</pre>"; 
+
+    // [One] => C++
+    // [Two] => Java
+    // [Three] => Python
+    // [Five] => C#
+    // [Six] => JavaScript
+
+
+$marge_three = [10=>"PHP",20=>"Java",30=>"Python"];
+$marge_four = [10=>"C++",40=>"C#",50=>"JavaScript"];
+echo "<pre>";
+print_r(array_merge($marge_three,$marge_four));
+echo "</pre>";
+
+    // [0] => PHP
+    // [1] => Java
+    // [2] => Python
+    // [3] => C++
+    // [4] => C#
+    // [5] => JavaScript
 
 
 
-// Current: Alice
-// Next: Bob
-// Current: Bob
-// Next: Charlie
-// Prev: Bob
-// Reset: Alice
-// End: Eve
-// Current: Eve
+$replace_one = ["One"=>"PHP","Two"=>"Java","Three"=>"Python"];
+$replace_two = ["One"=>"C++","Four"=>"C#","Five"=>"JavaScript"];    
+
+echo "<pre>";
+print_r(array_replace($replace_one,$replace_two));
+echo "</pre>";
+
+    // [One] => C++
+    // [Two] => Java
+    // [Three] => Python
+    // [Four] => C#
+    // [Five] => JavaScript
+
+$replace_three = [10=>"PHP",20=>"Java",30=>"Python"];
+$replace_four = [10=>"C++",40=>"C#",50=>"JavaScript"];
+echo "<pre>";   
+print_r(array_replace($replace_three,$replace_four));
+echo '</pre>'  ;
+
+    // [10] => C++
+    // [20] => Java
+    // [30] => Python
+    // [40] => C#
+    // [50] => JavaScript
+
+$array_rand = ["PHP","Java","Python","C++","C#"];
+
+echo $array_rand[array_rand($array_rand)];
+echo "<br>";
+
+//will return a random value from the array
+
+echo "<pre>";
+print_r(array_rand($array_rand,3));
+echo "</pre>";
+
+//will return 3 random keys from the array
+
+$array_shuffle = ["PHP","Java","Python","C++","C#"];
+shuffle($array_shuffle);
+echo "<pre>";
+print_r($array_shuffle);   
+echo "</pre>";
+
+//will shuffle the array and return the new array with random order
