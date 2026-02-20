5d4746b5bd9bcb5d1ec157aa88af2b318dea2e9f
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-21 00:10:38 +0100

    Filter validate part-1

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php
-//Filter List , Filter ID , Filter Var 
+//Filter var validate part-1 
 
-echo "<pre>";
-print_r(filter_list());
-echo "</pre>";
+$bool = "srgs";
+var_dump(filter_var($bool, FILTER_VALIDATE_BOOL,FILTER_NULL_ON_FAILURE));
+//if $bool = "any thing other True or False values it will returned NULL
 
-echo filter_id("boolean"); // 258 
-echo"<br>"; 
-$val = "on1" ;
+echo "<br>";
 
-// if (filter_var($val,258))
-if (filter_var($val,FILTER_VALIDATE_BOOL)){//use FILTER_VALIDATE_BOOL or use id = 258  
-echo "this is true ";
-}
-else {
-echo "this is false ";
-}
+$url = "https://www.youtube.com/";
+
+var_dump(filter_var($url,FILTER_VALIDATE_URL));
+//string(24) "https://www.youtube.com/" -> accept
+
+$url = "wqfsda";
+var_dump(filter_var($url,FILTER_VALIDATE_URL,FILTER_NULL_ON_FAILURE));
+//NULL 
+echo "<br>";
+
+$url = "https://www.youtube.com/?id=100";
+var_dump(filter_var($url,FILTER_VALIDATE_URL,[
+    "flags" => FILTER_NULL_ON_FAILURE | FILTER_FLAG_PATH_REQUIRED | FILTER_FLAG_QUERY_REQUIRED]));
+//string(31) "https://www.youtube.com/?id=100" -> accept the three 
+
+
+$ip = "192.168.1.1";
+var_dump(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_IPV4));
+//string(11) "192.168.1.1"
+
+
+$ip = "222";
+var_dump(filter_var($ip,FILTER_VALIDATE_IP,[
+    "flags" => FILTER_NULL_ON_FAILURE | FILTER_FLAG_IPV6]));
+// NULL
+
+$mac = "00:1A:2B:3C:4D:5E";
+var_dump(filter_var($mac,FILTER_VALIDATE_MAC));
+//  string(17) "00:1A:2B:3C:4D:5E"
